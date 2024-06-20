9e12d4a73750882edc3f948d098913628ba9548b
ibelieve3212 <70727665+ibelieve3212@users.noreply.github.com>
2024-06-21 02:28:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,8 +16,10 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+echo -i '$a src-git Openclash https://github.com/vernesong/OpenClash' feeds.conf.default
+echo -i '$a src-git DDNS-GO https://github.com/jeessy2/ddns-go' feeds.conf.default
 sed -i 's/vpn/services/g' package/zerotier/luci-app-zerotier/luasrc/controller/*.lua
 sed -i 's/vpn/services/g' package/zerotier/luci-app-zerotier/luasrc/model/cbi/zerotier/*.lua
 sed -i 's/vpn/services/g' package/zerotier/luci-app-zerotier/luasrc/view/zerotier/*.htm
